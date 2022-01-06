c4969ea4410b36ee7474b8c746a1505f4c108806
Bikram48 <bikram.chand@themegrill.com>
2022-01-06 17:21:24 +0545

    add - panel to the customizer

--- functions.php
+++ functions.php
@@ -1,7 +1,16 @@
 <?php
     function mytheme_customize_register( $wp_customize ) {
-        
+        $wp_customize -> add_panel( 'header_navigation_panel',
+        array(
+            'title' => 'Header Navigation Panel',
+            'description' => 'Adjust your header navigation',
+            'priority' => 40,
+            'capability' => 'edit_theme_option',
+            'theme_options' => '',
+            'active_callback' => ''
+        ) );
     }
+    
     add_action( 'customize_register', 'mytheme_customize_register' );
 
 
