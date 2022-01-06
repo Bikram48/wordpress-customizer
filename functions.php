90b4c8bbea0871c049161918bc61132f928f95ed
Bikram48 <bikram.chand@themegrill.com>
2022-01-06 23:19:10 +0545

    fix - not  displaying panel,section and control on customizer page

--- functions.php
+++ functions.php
@@ -1,53 +1,42 @@
 <?php
     function mytheme_customize_register( $wp_customize ) {
-        $wp_customize -> add_panel( 'header_navigation_panel',
+        
+        $wp_customize->add_panel( 'header_navigation_panel',
         array(
             'title' => 'Header Navigation Panel',
             'description' => 'Adjust your header navigation',
-            'priority' => 40,
-            'capability' => 'edit_theme_option',
-            'theme_options' => '',
-            'active_callback' => ''
+            'priority' => 160,
         ) );
 
-        $wp_customize -> add_section ( 'custom_control_section',
+        $wp_customize->add_section( 'custom_control_section',
         array(
             'title' => 'Custom Section',
             'description' => 'This is custom section',
+            'panel' => 'header_navigation_panel',
             'priority' => 40,
-            'capability' => 'edit_theme_option',
-            'theme_options' => '',
-            'active_callback' => '',
-            'description_hidden' => 'false',
         ) );
 
-        $wp_customize -> add_setting ( 'sample_default_text',
+        $wp_customize->add_setting( 'sample_default_text',
         array(
-            'default' => '',
+            'default' => '#081b4d',
             'transport' => 'refresh',
             'type' => 'theme_mod',
-            'capability' => 'edit_theme_option',
             'theme_supports' => '',
-            'validate_callback' => '',
-            'sanitize_callback' => '',
-            'sanitize_js_callback' => '',
-            'dirty' => false,
         ) );
 
-        $wp_customize -> add_control ( 'sample_default_text',
+        $wp_customize->add_control( 'sample_default_text',
         array(
             'label' => 'Default text control',
             'description' => 'This is a text type control',
             'section' => 'custom_control_section',
             'priority' => 10,
             'type' => 'text',
-            'capability' => 'edit_theme_option',
-            'input_attrs' => array(
-                'placeholder' => 'Enter full name'
-            ),
-        ) );
+            'capability' => 'edit_theme_options',
+        ));
     }
     
     add_action( 'customize_register', 'mytheme_customize_register' );
+
+
 
 
