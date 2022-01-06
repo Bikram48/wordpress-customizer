9f69ae66131aa778150a68ed3545271d4249e1da
Bikram48 <bikram.chand@themegrill.com>
2022-01-06 17:35:08 +0545

    add - section to the panel

--- functions.php
+++ functions.php
@@ -9,6 +9,17 @@
             'theme_options' => '',
             'active_callback' => ''
         ) );
+
+        $wp_customize -> add_section ( 'custom_control_section',
+        array(
+            'title' => 'Custom Section',
+            'description' => 'This is custom section',
+            'priority' => 40,
+            'capability' => 'edit_theme_option',
+            'theme_options' => '',
+            'active_callback' => '',
+            'description_hidden' => 'false',
+        ) );
     }
     
     add_action( 'customize_register', 'mytheme_customize_register' );
